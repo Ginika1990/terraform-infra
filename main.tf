b69b9a0f682767ba09edc6caf275b5b72d31f436
Ginikanwa Felicia Ebe <159577247+Ginika1990@users.noreply.github.com>
2025-05-26 14:32:58 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   source_image_reference {
   publisher = "Canonical"
   offer     = "UbuntuServer"
-  sku       = "20_04-lts"
+  sku       = "20.04-lts"
   version   = "latest"
 }
 

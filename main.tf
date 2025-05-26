53a26269efad10d2c2c6e3b42a5db771e814ca4f
Ginikanwa Felicia Ebe <159577247+Ginika1990@users.noreply.github.com>
2025-05-26 13:47:19 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 provider "azurerm" {
   features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 resource "azurerm_resource_group" "website_rg" {
@@ -72,12 +77,12 @@ resource "azurerm_network_interface" "nic" {
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                  = "website-vm"
-  resource_group_name   = azurerm_resource_group.website_rg.name
-  location              = azurerm_resource_group.website_rg.location
-  size                  = "Standard_B1s"
-  admin_username        = var.admin_username
-  admin_password        = var.admin_password
+  name                            = "website-vm"
+  resource_group_name             = azurerm_resource_group.website_rg.name
+  location                        = azurerm_resource_group.website_rg.location
+  size                            = "Standard_B1s"
+  admin_username                  = var.admin_username
+  admin_password                  = var.admin_password
   disable_password_authentication = false
 
   network_interface_ids = [azurerm_network_interface.nic.id]

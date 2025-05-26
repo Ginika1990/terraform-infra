02cfdbb9da1a5373bfd8693ea8e0ab834c97f556
Ginikanwa Felicia Ebe <159577247+Ginika1990@users.noreply.github.com>
2025-05-26 13:38:51 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,17 @@
-variable "admin_username" {
-  type        = string
-  description = "The admin username for the VM"
+variable "subscription_id" {
+  type = string
 }
 
-variable "admin_password" {
-  type        = string
-  sensitive   = true
-  description = "The admin password for the VM"
+variable "client_id" {
+  type = string
 }
+
+variable "client_secret" {
+  type = string
+  sensitive = true
+}
+
+variable "tenant_id" {
+  type = string
+}
+
